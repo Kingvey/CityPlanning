77fe9355b93be58b92cb4ed671244bfff23c68be
wucan <wucangeo@gmail.com>
2014-12-27 09:49:39 +0800

    测试时，取消启动窗口渐变
    节省时间

--- CityPlanning/frmStartConnectionConfig.cs
+++ CityPlanning/frmStartConnectionConfig.cs
@@ -45,20 +45,7 @@ namespace CityPlanning
         //窗体关闭
         private void frmStartConnectionConfig_FormClosed(object sender, FormClosedEventArgs e)
         {
-            //FTP
-            FTPConnection.FtpIP = this.txt_ftpIP.Text.Trim();
-            FTPConnection.FtpPort = this.txt_ftpPort.Text.Trim();
-            FTPConnection.FtpUserName = this.txt_ftpUserName.Text.Trim();
-            FTPConnection.FtpPassword = this.txt_ftpPassword.Text.Trim();
-            //数据库            
-            SQLServerConnection.DBServerName = this.txt_DBServerName.Text.Trim();
-            SQLServerConnection.DbCatalogName = this.txt_DBCatalogName.Text.Trim();
-            SQLServerConnection.DBUserName = this.txt_DBUserName.Text.Trim();
-            SQLServerConnection.DBPassword = this.txt_DBPassword.Text.Trim();
-            //用户
-            UserManager.SysUserName = this.txt_SysUserName.Text.Trim();
-            UserManager.SysPassword = this.txt_SysPassword.Text.Trim();
-                        
+            
             this.timer1.Stop();
             //如果点击取消按钮，则mainFrm已经被释放
             if (mainFrm != null)
@@ -73,8 +60,32 @@ namespace CityPlanning
         //登录按钮
         private void btn_Login_Click(object sender, EventArgs e)
         {
-            this.timer1.Interval = 100;
-            this.timer1.Start(); 
+            //FTP
+            FTPConnection.FtpIP = this.txt_ftpIP.Text.Trim();
+            FTPConnection.FtpPort = this.txt_ftpPort.Text.Trim();
+            FTPConnection.FtpUserName = this.txt_ftpUserName.Text.Trim();
+            FTPConnection.FtpPassword = this.txt_ftpPassword.Text.Trim();
+            //数据库            
+            SQLServerConnection.DBServerName = this.txt_DBServerName.Text.Trim();
+            SQLServerConnection.DbCatalogName = this.txt_DBCatalogName.Text.Trim();
+            SQLServerConnection.DBUserName = this.txt_DBUserName.Text.Trim();
+            SQLServerConnection.DBPassword = this.txt_DBPassword.Text.Trim();
+            //用户
+            UserManager.SysUserName = this.txt_SysUserName.Text.Trim();
+            UserManager.SysPassword = this.txt_SysPassword.Text.Trim();
+            
+
+            if (mainFrm != null)
+            {
+                //测试时，取消渐变
+                //this.timer1.Interval = 100;
+                //this.timer1.Start();
+                this.Close();
+            }
+            else
+            {
+                this.Close();
+            }
         }
 
         //取消按钮
